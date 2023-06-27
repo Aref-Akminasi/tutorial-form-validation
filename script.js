6ff73b278ef20adb28b9a7b155bd476e18e69b01
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-06-27 11:10:06 +0200

    Created a validation for the username and password

--- script.js
+++ script.js
@@ -17,6 +17,14 @@ function checkInputs() {
   const passwordValue = password.value.trim();
   const repeatPasswordValue = repeatPassword.value.trim();
 
+  if (usernameValue === '') {
+    setErrorFor(username, 'Username cannot be blank');
+  } else if (!isUsername(usernameValue)) {
+    setErrorFor(username, 'Letters only, 4+ characters');
+  } else {
+    setSuccessFor(username);
+  }
+
   if (emailValue === '') {
     setErrorFor(email, 'Email cannot be blank');
   } else if (!isEmail(emailValue)) {
@@ -25,20 +33,19 @@ function checkInputs() {
     setSuccessFor(email);
   }
 
-  if (usernameValue === '') {
-    setErrorFor(username, 'Username cannot be blank');
-  } else {
-    setSuccessFor(username);
-  }
-
   if (passwordValue === '') {
     setErrorFor(password, 'Password cannot be blank');
+  } else if (!isPassword(passwordValue)) {
+    setErrorFor(password, 'a, A, 0-9, @, longer than 8');
   } else {
     setSuccessFor(password);
   }
 
   if (repeatPasswordValue === '') {
     setErrorFor(repeatPassword, 'Password cannot be blank');
+  } else if (password.parentElement.classList.contains('error')) {
+    //If the password is not valid, the repeated password should be an error too.
+    setErrorFor(repeatPassword, 'Password is not valid');
   } else if (passwordValue !== repeatPasswordValue) {
     setErrorFor(repeatPassword, 'Passwords do not match');
   } else {
@@ -56,7 +63,13 @@ function checkInputs() {
     validation.style.display = 'none';
   }
 
-  /* Set error and set succes will target the parent element of the input which has the form control */
+  /* Set success and error succes will target the parent element of the input which has the form control */
+
+  function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+  }
+
   function setErrorFor(input, message) {
     const formControl = input.parentElement; // .form-control
     const small = formControl.querySelector('small');
@@ -64,14 +77,30 @@ function checkInputs() {
     formControl.className = 'form-control error';
   }
 
-  function setSuccessFor(input) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control success';
-  }
+  /* The functions below are responsible for the validation of the inputs when the submit button is clicked */
+  /* There is no function defined for the 'Repeat password' input because it has to be the same as the password so the same criteria is applied */
 
   function isEmail(email) {
     return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
       email
+    );
+  }
+
+  function isUsername(username) {
+    return (
+      (/[A-Z]/.test(username) || /[a-z]/.test(username)) &&
+      !/[0-9]/.test(username) &&
+      !/[!@#$%^&*]/.test(username) &&
+      username.length >= 4
+    );
+  }
+  function isPassword(password) {
+    return (
+      /[A-Z]/.test(password) &&
+      /[a-z]/.test(password) &&
+      /[0-9]/.test(password) &&
+      /[!@#$%^&*]/.test(password) &&
+      password.length >= 8
     );
   }
 }
